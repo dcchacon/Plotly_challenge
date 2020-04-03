c56008ee7f300ae860d95b17875e740d60171039
Diana Chacon <dcmedina@gmail.com>
2020-04-03 09:25:31 -0700

    20200403 at 9:34 starting code to grab data

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,29 @@
 // D3 library to read from json file
-d3.json("Data/samples.json").then((data)=>{
+//const buildChart = (someSampleID) => {
+d3.json("samples.json").then((data) => {
+    const sample = "940"
     // use sample_values as the values for the bar chart
     console.log(data)
-    // var sample_values = {
+    const samples = data.samples
+    var resultArray = samples.filter(sampleObject => sampleObject.id === sample)
+    var result = resultArray[0]
 
-        
-    // }
-// use otu_ids as the labels for the bar chart
 
-// use otu_labels as the hovertext for the chart
+    // use otu_ids as the labels for the bar chart
+    // use otu_labels as the hovertext for the chart
+    var otu_ids = result.otu_ids
+    var otu_labels = result.otu_labels
+    var sample_values = result.sample_values
+
+//Graph
+var Graph = {
+    labels: otu_labels,
+    values: sample_values,
+    
+}
 
 }).catch(err => console.log(err));
+//}
 
 
 
